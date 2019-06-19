6c69dd77465538a1f034ce0d470cd5bab48e1011
wiemrh <you@ouiemm.je@gmail.com>
2019-06-19 16:36:38 -0700

    version Finale

---
+++ ExamenRatrappage/DetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailsViewController.swift
+//  ExamenRatrappage
+//
+//  Created by +	wiem rebah  on 19/06/2019.
+//  Copyright © 2019 +	wiem rebah . All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
